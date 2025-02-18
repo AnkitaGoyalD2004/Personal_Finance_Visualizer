2d3978e592c425bb8df5b70d7494ee9095d87786
Ankita Goyal <goyalankita0325@gmail.com>
2025-02-18 22:54:24 +0530

    update

--- models/Budget.ts
+++ models/Budget.ts
@@ -1,8 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
 
-const BudgetSchema = new mongoose.Schema({
-  category: { type: String, required: true, unique: true },
+export interface Budget extends Document {
+  category: string;
+  amount: number;
+  month: string; // Format: YYYY-MM
+}
+
+const BudgetSchema = new Schema<Budget>({
+  category: { type: String, required: true },
   amount: { type: Number, required: true },
+  month: { type: String, required: true },
 });
 
-export default mongoose.models.Budget || mongoose.model('Budget', BudgetSchema);
+export default mongoose.models.Budget || mongoose.model<Budget>("Budget", BudgetSchema);
